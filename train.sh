222bda16abfd795909fdd92fd27aa0b8b3857dcf
flyingsheep <44082512+flyingsheepbin@users.noreply.github.com>
2022-05-30 21:59:30 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,23 +1,24 @@
 # step 1:训练生成伪标签模型
 cd pseudo_produce
 python train.py
+python pseudo.py
 
 # step 2:训练一阶段四个模型
 
 # model1: b7ns
 cd ../b7ns_stage1
-python train.py 
+python train.py
 
 # model2: swin384
 cd ../swin384_stage1
-python train.py
+python train.py 
 
 # model3: effv2-large
 cd ../v2_stage1
-python train.py
+python train.py -
 
 # model4: swin224
-cd swin224_stage1
+cd ../swin224_stage1
 python train.py
 
 
@@ -25,16 +26,16 @@ python train.py
 
 # model1: b7ns
 cd ../b7ns_stage2
-python train.py
+python train.py 
 
 # model2: swin384
 cd ../swin384_stage2
-python train.py
+python train.py 
 
 # model3: effv2-large
 cd ../v2_stage2
 python train.py
 
 # model4: swin224
-# cd
-# python train.py
+cd ../swin224_stage2
+python train.py  
